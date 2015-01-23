f27d710a34713126acf5ed1505ea6baa0f5a73cb
Ben Woods <bwoods@stchas.edu>
2015-01-23 11:43:39 -0600

    Adding zpool class for zfs module

---
+++ manifests/zpool.pp
@@ -0,0 +1,12 @@
+class zfsmodule::zpool (
+  $pool = hiera('zfsmodule::zpool::pool'),
+  $ensure = hiera('zfsmodule::zpool::ensure', 'present'),
+  $raid_parity = hiera('zfsmodule::zpool::raid_parity'),
+  $raidz = hiera('zfsmodule::zpool::raidz'),
+) {
+    zpool { $pool:
+      ensure => $ensure,
+      raid_parity => $raid_parity,
+      raidz => $raidz,
+    }
+}
